c36f8f41fc263315370d5abbae61ba89904e3f4e
Michael Lazarevic <GL15010793@HAL.local>
2019-02-20 16:53:43 +0000

    Asteroids added to screen and collision list

--- app/src/main/java/com/northumbria/en0618/AsteroidManager.java
+++ app/src/main/java/com/northumbria/en0618/AsteroidManager.java
@@ -3,14 +3,23 @@ package com.northumbria.en0618;
 import android.support.annotation.DrawableRes;
 
 import com.northumbria.en0618.engine.Game;
+import com.northumbria.en0618.engine.Input;
 
 import java.util.ArrayList;
 import java.util.List;
 
 public class AsteroidManager {
 
+    private static final float ASTEROID_SIZE = 75.0f;
+    private static final int ASTEROIDS_PER_BARRIER = 3;
+    private static final int NUMBER_OF_BARRIERS = 2;
+
+    private float FIRST_BARRIER_X;
+    private float SECOND_BARRIER_X;
+
     private static final @DrawableRes
-    int[] ALIEN_SPRITE_DRAWABLE_IDS = new int[]
+
+    int[] ASTEROID_SPRITE_DRAWABLE_IDS = new int[]
             {
                     R.drawable.asteroid_1,
                     R.drawable.asteroid_2,
@@ -19,8 +28,6 @@ public class AsteroidManager {
                     R.drawable.asteroid_5,
                     R.drawable.asteroid_6
             };
-
-    private List m_asteroidList = new ArrayList<>();
 
     private Game m_game;
     private CollisionLists m_colList;
@@ -31,10 +38,33 @@ public class AsteroidManager {
     {
         m_colList = collisionList;
         m_game = game;
+        FIRST_BARRIER_X = Input.getScreenWidth() * 0.33f;
+        SECOND_BARRIER_X = Input.getScreenWidth() * 0.66f;
     }
 
     void createAsteroids()
     {
+        //TODO - CONSTANTS / RETHINKING
+        float baseXModifier = 85.0f; // NEEDS RETHINKING, CURRENTLY REMOVED FROM BASE X, THEN ADDED * I
+        float baseYPosition = 320.0f; // BASE Y POSITION, MAKE CONSTANT
+        float asteroidYModifier = -40.0f; // Disjoints the base Y position
 
+        for(int i = 0; i < ASTEROIDS_PER_BARRIER; i++)
+        {
+            Asteroid tempFirstAsteroid = new Asteroid(m_game.getActivity(),
+                    ASTEROID_SPRITE_DRAWABLE_IDS[i], FIRST_BARRIER_X - baseXModifier + (baseXModifier * i),
+                    baseYPosition + asteroidYModifier, ASTEROID_SIZE);
+            m_game.addGameObject(tempFirstAsteroid);
+            m_colList.addAsteroid(tempFirstAsteroid);
+
+
+            Asteroid tempSecondAsteroid = new Asteroid(m_game.getActivity(),
+                    ASTEROID_SPRITE_DRAWABLE_IDS[5-i], SECOND_BARRIER_X - baseXModifier + (baseXModifier * i),
+                    baseYPosition + asteroidYModifier, ASTEROID_SIZE);
+            m_game.addGameObject(tempSecondAsteroid);
+            m_colList.addAsteroid(tempSecondAsteroid);
+
+            asteroidYModifier = -asteroidYModifier; // Flips so that asteroids spawn down, Up, down, Up
+        }
     }
 }
